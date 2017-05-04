1543c90dd661502f475f5def7e89fdb12d1aabd6
Hunter Kleindl <hkleindl@gmail.com>
2017-05-04 23:44:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var lis = document.getElementById('app').querySelectorAll('ul ranked-list li')
+  for (var i = 0; i < lis.length; i++) {
+  
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
